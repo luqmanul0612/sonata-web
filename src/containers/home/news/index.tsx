a51b53f3075676ff59670d8d81026c9103ee4055
Muhammad Luqmanul Hakim <iamhkmid@gmail.com>
2025-05-03 21:05:52 +0700

    update

--- src/containers/home/news/index.tsx
+++ src/containers/home/news/index.tsx
@@ -3,7 +3,7 @@ import classNames from "./news.module.scss";
 const News = () => {
   return (
     <section id="news" className={classNames.main}>
-      News
+      <div className={classNames.content}>a</div>
     </section>
   );
 };
