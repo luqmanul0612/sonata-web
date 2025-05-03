ec7391c6c9ed758c6c5b6929995fff2fbd2e4304
Muhammad Luqmanul Hakim <iamhkmid@gmail.com>
2025-05-03 21:51:26 +0700

    update

---
+++ src/containers/home/news/data.ts
@@ -0,0 +1,28 @@
+import newsImage from "@/assets/images/news-image-example.jpg";
+
+export const newsData = [
+  {
+    key: 1,
+    title: "Judul Berita",
+    date: "Senin, 27 April 2025",
+    image: newsImage,
+    content:
+      "Lorem ipsum dolor sit amet consectetur. Tellus viverra a id pellentesque eget vulputate. Vitae amet justo mi magna morbi montes vel amet. Bibendum pellentesque risus metus sagittis. Nisl sed tincidunt euismod habitasse vulputate quam donec morbi dictumst. Mauris rutrum porttitor et sit eget venenatis leo et. Posuere justo sed tristique viverra a. Quam velit faucibus sed eu leo sed sit.",
+  },
+  {
+    key: 2,
+    title: "Judul Berita",
+    date: "Senin, 27 April 2025",
+    image: newsImage,
+    content:
+      "Lorem ipsum dolor sit amet consectetur. Tellus viverra a id pellentesque eget vulputate. Vitae amet justo mi magna morbi montes vel amet. Bibendum pellentesque risus metus sagittis. Nisl sed tincidunt euismod habitasse vulputate quam donec morbi dictumst. Mauris rutrum porttitor et sit eget venenatis leo et. Posuere justo sed tristique viverra a. Quam velit faucibus sed eu leo sed sit.",
+  },
+  {
+    key: 3,
+    title: "Judul Berita",
+    date: "Senin, 27 April 2025",
+    image: newsImage,
+    content:
+      "Lorem ipsum dolor sit amet consectetur. Tellus viverra a id pellentesque eget vulputate. Vitae amet justo mi magna morbi montes vel amet. Bibendum pellentesque risus metus sagittis. Nisl sed tincidunt euismod habitasse vulputate quam donec morbi dictumst. Mauris rutrum porttitor et sit eget venenatis leo et. Posuere justo sed tristique viverra a. Quam velit faucibus sed eu leo sed sit.",
+  },
+];
--- src/containers/home/news/index.tsx
+++ src/containers/home/news/index.tsx
@@ -1,9 +1,43 @@
+"use client";
+
+import { useTranslation } from "react-i18next";
 import classNames from "./news.module.scss";
+import { newsData } from "./data";
+import Image from "next/image";
+import Clock from "@/assets/icons/clock.svg";
 
 const News = () => {
+  const { t } = useTranslation();
   return (
     <section id="news" className={classNames.main}>
-      <div className={classNames.content}>a</div>
+      <div className={classNames.content}>
+        <p className={classNames.title}>{t("Latest News")}</p>
+        <div className={classNames["news-wrapper"]}>
+          {newsData.map((item) => (
+            <div className={classNames.news} key={item.key}>
+              <div className={classNames.image}>
+                <Image
+                  src={item.image}
+                  alt="images"
+                  fill
+                  style={{ objectFit: "cover" }}
+                />
+              </div>
+              <div className={classNames.info}>
+                <div className={classNames.date}>
+                  <Clock className={classNames.icon} />
+                  <p className={classNames.text}>{item.date}</p>
+                </div>
+                <p className={classNames.title}>{item.title}</p>
+                <p className={classNames.content}>{item.content}</p>
+                <a href="" className={classNames.more}>
+                  {t("Read more")}...
+                </a>
+              </div>
+            </div>
+          ))}
+        </div>
+      </div>
     </section>
   );
 };
