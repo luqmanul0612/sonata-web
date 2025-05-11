758946a0c67272b805fb0563b45d44cedea243f1
Muhammad Luqmanul Hakim <iamhkmid@gmail.com>
2025-05-11 12:48:43 +0700

    update

--- src/containers/news/data.ts
+++ src/containers/news/data.ts
@@ -3,43 +3,115 @@ import newsImage2 from "@/assets/images/news/news-image-example-2.webp";
 
 export const newsData = [
   {
-    key: 1,
-    title: "Judul Berita",
-    date: "Senin, 27 April 2025",
-    image: newsImage1,
-    content:
-      "Lorem ipsum dolor sit amet consectetur. Tellus viverra a id pellentesque eget vulputate. Vitae amet justo mi magna morbi montes vel amet. Bibendum pellentesque risus metus sagittis. Nisl sed tincidunt euismod habitasse vulputate quam donec morbi dictumst. Mauris rutrum porttitor et sit eget venenatis leo et. Posuere justo sed tristique viverra a. Quam velit faucibus sed eu leo sed sit.",
+    key: "latest",
+    label: "Latest",
+    items: [
+      {
+        key: 1,
+        title: "Judul Berita",
+        date: "Senin, 27 April 2025",
+        image: newsImage1,
+        content:
+          "Lorem ipsum dolor sit amet consectetur. Tellus viverra a id pellentesque eget vulputate. Vitae amet justo mi magna morbi montes vel amet. Bibendum pellentesque risus metus sagittis. Nisl sed tincidunt euismod habitasse vulputate quam donec morbi dictumst. Mauris rutrum porttitor et sit eget venenatis leo et. Posuere justo sed tristique viverra a. Quam velit faucibus sed eu leo sed sit.",
+      },
+      {
+        key: 2,
+        title: "Judul Berita",
+        date: "Senin, 27 April 2025",
+        image: newsImage2,
+        content:
+          "Lorem ipsum dolor sit amet consectetur. Tellus viverra a id pellentesque eget vulputate. Vitae amet justo mi magna morbi montes vel amet. Bibendum pellentesque risus metus sagittis. Nisl sed tincidunt euismod habitasse vulputate quam donec morbi dictumst. Mauris rutrum porttitor et sit eget venenatis leo et. Posuere justo sed tristique viverra a. Quam velit faucibus sed eu leo sed sit.",
+      },
+      {
+        key: 3,
+        title: "Judul Berita",
+        date: "Senin, 27 April 2025",
+        image: newsImage2,
+        content:
+          "Lorem ipsum dolor sit amet consectetur. Tellus viverra a id pellentesque eget vulputate. Vitae amet justo mi magna morbi montes vel amet. Bibendum pellentesque risus metus sagittis. Nisl sed tincidunt euismod habitasse vulputate quam donec morbi dictumst. Mauris rutrum porttitor et sit eget venenatis leo et. Posuere justo sed tristique viverra a. Quam velit faucibus sed eu leo sed sit.",
+      },
+      {
+        key: 4,
+        title: "Judul Berita",
+        date: "Senin, 27 April 2025",
+        image: newsImage2,
+        content:
+          "Lorem ipsum dolor sit amet consectetur. Tellus viverra a id pellentesque eget vulputate. Vitae amet justo mi magna morbi montes vel amet. Bibendum pellentesque risus metus sagittis. Nisl sed tincidunt euismod habitasse vulputate quam donec morbi dictumst. Mauris rutrum porttitor et sit eget venenatis leo et. Posuere justo sed tristique viverra a. Quam velit faucibus sed eu leo sed sit.",
+      },
+      {
+        key: 5,
+        title: "Judul Berita",
+        date: "Senin, 27 April 2025",
+        image: newsImage2,
+        content:
+          "Lorem ipsum dolor sit amet consectetur. Tellus viverra a id pellentesque eget vulputate. Vitae amet justo mi magna morbi montes vel amet. Bibendum pellentesque risus metus sagittis. Nisl sed tincidunt euismod habitasse vulputate quam donec morbi dictumst. Mauris rutrum porttitor et sit eget venenatis leo et. Posuere justo sed tristique viverra a. Quam velit faucibus sed eu leo sed sit.",
+      },
+    ],
   },
   {
-    key: 2,
-    title: "Judul Berita",
-    date: "Senin, 27 April 2025",
-    image: newsImage2,
-    content:
-      "Lorem ipsum dolor sit amet consectetur. Tellus viverra a id pellentesque eget vulputate. Vitae amet justo mi magna morbi montes vel amet. Bibendum pellentesque risus metus sagittis. Nisl sed tincidunt euismod habitasse vulputate quam donec morbi dictumst. Mauris rutrum porttitor et sit eget venenatis leo et. Posuere justo sed tristique viverra a. Quam velit faucibus sed eu leo sed sit.",
+    key: "event",
+    label: "Event",
+    items: [
+      {
+        key: 2,
+        title: "Judul Berita",
+        date: "Senin, 27 April 2025",
+        image: newsImage2,
+        content:
+          "Lorem ipsum dolor sit amet consectetur. Tellus viverra a id pellentesque eget vulputate. Vitae amet justo mi magna morbi montes vel amet. Bibendum pellentesque risus metus sagittis. Nisl sed tincidunt euismod habitasse vulputate quam donec morbi dictumst. Mauris rutrum porttitor et sit eget venenatis leo et. Posuere justo sed tristique viverra a. Quam velit faucibus sed eu leo sed sit.",
+      },
+      {
+        key: 1,
+        title: "Judul Berita",
+        date: "Senin, 27 April 2025",
+        image: newsImage1,
+        content:
+          "Lorem ipsum dolor sit amet consectetur. Tellus viverra a id pellentesque eget vulputate. Vitae amet justo mi magna morbi montes vel amet. Bibendum pellentesque risus metus sagittis. Nisl sed tincidunt euismod habitasse vulputate quam donec morbi dictumst. Mauris rutrum porttitor et sit eget venenatis leo et. Posuere justo sed tristique viverra a. Quam velit faucibus sed eu leo sed sit.",
+      },
+    ],
   },
   {
-    key: 3,
-    title: "Judul Berita",
-    date: "Senin, 27 April 2025",
-    image: newsImage2,
-    content:
-      "Lorem ipsum dolor sit amet consectetur. Tellus viverra a id pellentesque eget vulputate. Vitae amet justo mi magna morbi montes vel amet. Bibendum pellentesque risus metus sagittis. Nisl sed tincidunt euismod habitasse vulputate quam donec morbi dictumst. Mauris rutrum porttitor et sit eget venenatis leo et. Posuere justo sed tristique viverra a. Quam velit faucibus sed eu leo sed sit.",
+    key: "projects",
+    label: "Projects",
+    items: [
+      {
+        key: 1,
+        title: "Judul Berita",
+        date: "Senin, 27 April 2025",
+        image: newsImage1,
+        content:
+          "Lorem ipsum dolor sit amet consectetur. Tellus viverra a id pellentesque eget vulputate. Vitae amet justo mi magna morbi montes vel amet. Bibendum pellentesque risus metus sagittis. Nisl sed tincidunt euismod habitasse vulputate quam donec morbi dictumst. Mauris rutrum porttitor et sit eget venenatis leo et. Posuere justo sed tristique viverra a. Quam velit faucibus sed eu leo sed sit.",
+      },
+      {
+        key: 2,
+        title: "Judul Berita",
+        date: "Senin, 27 April 2025",
+        image: newsImage2,
+        content:
+          "Lorem ipsum dolor sit amet consectetur. Tellus viverra a id pellentesque eget vulputate. Vitae amet justo mi magna morbi montes vel amet. Bibendum pellentesque risus metus sagittis. Nisl sed tincidunt euismod habitasse vulputate quam donec morbi dictumst. Mauris rutrum porttitor et sit eget venenatis leo et. Posuere justo sed tristique viverra a. Quam velit faucibus sed eu leo sed sit.",
+      },
+      {
+        key: 3,
+        title: "Judul Berita",
+        date: "Senin, 27 April 2025",
+        image: newsImage1,
+        content:
+          "Lorem ipsum dolor sit amet consectetur. Tellus viverra a id pellentesque eget vulputate. Vitae amet justo mi magna morbi montes vel amet. Bibendum pellentesque risus metus sagittis. Nisl sed tincidunt euismod habitasse vulputate quam donec morbi dictumst. Mauris rutrum porttitor et sit eget venenatis leo et. Posuere justo sed tristique viverra a. Quam velit faucibus sed eu leo sed sit.",
+      },
+    ],
   },
   {
-    key: 4,
-    title: "Judul Berita",
-    date: "Senin, 27 April 2025",
-    image: newsImage2,
-    content:
-      "Lorem ipsum dolor sit amet consectetur. Tellus viverra a id pellentesque eget vulputate. Vitae amet justo mi magna morbi montes vel amet. Bibendum pellentesque risus metus sagittis. Nisl sed tincidunt euismod habitasse vulputate quam donec morbi dictumst. Mauris rutrum porttitor et sit eget venenatis leo et. Posuere justo sed tristique viverra a. Quam velit faucibus sed eu leo sed sit.",
-  },
-  {
-    key: 5,
-    title: "Judul Berita",
-    date: "Senin, 27 April 2025",
-    image: newsImage2,
-    content:
-      "Lorem ipsum dolor sit amet consectetur. Tellus viverra a id pellentesque eget vulputate. Vitae amet justo mi magna morbi montes vel amet. Bibendum pellentesque risus metus sagittis. Nisl sed tincidunt euismod habitasse vulputate quam donec morbi dictumst. Mauris rutrum porttitor et sit eget venenatis leo et. Posuere justo sed tristique viverra a. Quam velit faucibus sed eu leo sed sit.",
+    key: "humanity",
+    label: "Humanity",
+    items: [
+      {
+        key: 1,
+        title: "Judul Berita",
+        date: "Senin, 27 April 2025",
+        image: newsImage1,
+        content:
+          "Lorem ipsum dolor sit amet consectetur. Tellus viverra a id pellentesque eget vulputate. Vitae amet justo mi magna morbi montes vel amet. Bibendum pellentesque risus metus sagittis. Nisl sed tincidunt euismod habitasse vulputate quam donec morbi dictumst. Mauris rutrum porttitor et sit eget venenatis leo et. Posuere justo sed tristique viverra a. Quam velit faucibus sed eu leo sed sit.",
+      },
+    ],
   },
 ];
--- src/containers/news/index.tsx
+++ src/containers/news/index.tsx
@@ -4,6 +4,9 @@ import Image from "next/image";
 import classNames from "./news.module.scss";
 import clientsMain from "@/assets/images/news/news-main-image.webp";
 import { useTranslation } from "react-i18next";
+import { newsData } from "./data";
+import * as Tabs from "@radix-ui/react-tabs";
+import NewsCard from "@/components/molecules/news-card";
 
 const NewsContainer = () => {
   const { t } = useTranslation();
@@ -19,6 +22,35 @@ const NewsContainer = () => {
             style={{ objectFit: "cover" }}
           />
         </div>
+        <Tabs.Root className={classNames.tabs} defaultValue="latest">
+          <Tabs.List
+            className={classNames["tabs-list"]}
+            aria-label="Manage your account"
+          >
+            {newsData.map((item) => (
+              <Tabs.Trigger
+                key={item.key}
+                className={classNames["tabs-trigger"]}
+                value={item.key}
+              >
+                {t(item.label)}
+              </Tabs.Trigger>
+            ))}
+          </Tabs.List>
+          {newsData.map((item) => (
+            <Tabs.Content
+              key={item.key}
+              className={classNames["tabs-content"]}
+              value={item.key}
+            >
+              <div className={classNames["news-wrapper"]}>
+                {item.items.map((news) => (
+                  <NewsCard key={news.key} data={news} />
+                ))}
+              </div>
+            </Tabs.Content>
+          ))}
+        </Tabs.Root>
       </div>
     </div>
   );
