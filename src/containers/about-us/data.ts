66a6952456821d3b51f638d54fd1547499fc1320
Muhammad Luqmanul Hakim <iamhkmid@gmail.com>
2025-05-11 14:02:53 +0700

    update

---
+++ src/containers/about-us/data.ts
@@ -0,0 +1,75 @@
+import rubencurtis from "@/assets/images/about/teams/ruben-curtis.webp";
+import alfredoekstrombothman from "@/assets/images/about/teams/alfredo-ekstrom-bothman.webp";
+import gianavetrovs from "@/assets/images/about/teams/giana-vetrovs.webp";
+import jordynworkman from "@/assets/images/about/teams/jordyn-workman.webp";
+import skylarlevin from "@/assets/images/about/teams/skylar-levin.webp";
+import kadinsiphron from "@/assets/images/about/teams/kadin-shiphron.webp";
+import skylardokidis from "@/assets/images/about/teams/skylar-dokidis.webp";
+import ryannbergson from "@/assets/images/about/teams/ryan-bergson.webp";
+import rogerpress from "@/assets/images/about/teams/roger-press.webp";
+import marleysiphron from "@/assets/images/about/teams/marley-shipron.webp";
+import rogersaris from "@/assets/images/about/teams/roger-saris.webp";
+import changerhielmadsen from "@/assets/images/about/teams/change-rhiel-madsen.webp";
+
+export const teamsData = [
+  {
+    name: "Ruben Curtis",
+    position: "Project Manager (PM)",
+    image: rubencurtis,
+  },
+  {
+    name: "Alfredo Ekstrom Bothman",
+    position: "Deputy Project Manager",
+    image: alfredoekstrombothman,
+  },
+  {
+    name: "Giana Vetrovs",
+    position: "Project Control Engineer",
+    image: gianavetrovs,
+  },
+  {
+    name: "Jordyn Workman",
+    position: "Cost Controller",
+    image: jordynworkman,
+  },
+  {
+    name: "Skylar Levin",
+    position: "Scheduler / Planner",
+    image: skylarlevin,
+  },
+  {
+    name: "Kadin Siphron",
+    position: "Document Controller",
+    image: kadinsiphron,
+  },
+  {
+    name: "Skylar Dokidis",
+    position: "Contract Administrator",
+    image: skylardokidis,
+  },
+  {
+    name: "Ryan Bergson",
+    position: "Procurement Specialist",
+    image: ryannbergson,
+  },
+  {
+    name: "Roger Press",
+    position: "HSE Manager / Officer",
+    image: rogerpress,
+  },
+  {
+    name: "Marley Siphron",
+    position: "Risk Management ",
+    image: marleysiphron,
+  },
+  {
+    name: "Roger Saris",
+    position: "Specialist",
+    image: rogersaris,
+  },
+  {
+    name: "Chance Rhiel Madsen",
+    position: "Mechanical Engineer",
+    image: changerhielmadsen,
+  },
+];
--- src/containers/about-us/index.tsx
+++ src/containers/about-us/index.tsx
@@ -6,6 +6,7 @@ import mainImage from "@/assets/images/about/about-main-image.svg?url";
 import organizationCarts from "@/assets/images/about/organization-carts.webp";
 import { Trans, useTranslation } from "react-i18next";
 import GetQuote from "@/components/molecules/get-quote";
+import { teamsData } from "./data";
 
 const AboutUsContainer = () => {
   const { i18n } = useTranslation();
@@ -41,6 +42,31 @@ const AboutUsContainer = () => {
             className={classNames.img}
             style={{ objectFit: "cover" }}
           />
+        </div>
+        <div className={classNames["our-teams"]}>
+          <p className={classNames.title}>{i18n.t("Our Teams")}</p>
+          <p className={classNames.description}>
+            {i18n.t("We have many teams from different divisions")}
+          </p>
+          <div className={classNames["teams-wrapper"]}>
+            {teamsData.map((item) => (
+              <div className={classNames.person} key={item.name}>
+                <div className={classNames.image}>
+                  <Image
+                    src={item.image}
+                    alt="images"
+                    fill
+                    style={{ objectFit: "cover" }}
+                    className={classNames.img}
+                  />
+                </div>
+                <div className={classNames.info}>
+                  <p className={classNames.name}>{item.name}</p>
+                  <p className={classNames.position}>{item.position}</p>
+                </div>
+              </div>
+            ))}
+          </div>
         </div>
         <GetQuote />
       </div>
