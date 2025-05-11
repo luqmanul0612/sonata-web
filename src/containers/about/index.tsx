5d535b49df8fc0830dbf852313e08d7da862e19a
Muhammad Luqmanul Hakim <iamhkmid@gmail.com>
2025-05-11 13:24:01 +0700

    update

--- src/containers/about/index.tsx
+++ src/containers/about/index.tsx
@@ -3,8 +3,10 @@
 import Image from "next/image";
 import classNames from "./about.module.scss";
 import mainImage from "@/assets/images/about/about-main-image.svg?url";
+import { Trans, useTranslation } from "react-i18next";
 
 const AboutContainer = () => {
+  const { i18n } = useTranslation();
   return (
     <div className={classNames.main}>
       <div className={classNames.content}>
@@ -15,6 +17,18 @@ const AboutContainer = () => {
             fill
             className={classNames.img}
             style={{ objectFit: "cover" }}
+          />
+        </div>
+        <div className={classNames["text-content"]}>
+          <Trans
+            key={i18n.language}
+            i18nKey="about_text"
+            components={{
+              companyname: <p className={classNames.companyname} />,
+              subtitle: <p className={classNames.subtitle} />,
+              br: <br />,
+              text: <p className={classNames.text} />,
+            }}
           />
         </div>
       </div>
