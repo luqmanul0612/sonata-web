51550b439ea7fa6f1a7f40f67850df6a45fbb1dd
Muhammad Luqmanul Hakim <iamhkmid@gmail.com>
2025-05-12 20:18:43 +0700

    update

--- src/containers/certificates/data.ts
+++ src/containers/certificates/data.ts
@@ -4,21 +4,24 @@ import certificateImage3 from "@/assets/images/certificates/certificate-award-3.
 
 export const certificatesData = [
   {
-    name: "Judul Sertifikat",
+    key: 1,
+    title: "Judul Sertifikat",
     date: "12 Mei 2025",
     description:
       "Lorem ipsum dolor sit amet consectetur. Tellus viverra a id pellentesque eget vulputate. Vitae amet justo mi magna morbi montes vel amet. Bibendum pellentesque risus metus sagittis. Nisl sed tincidunt euismod habitasse vulputate quam donec morbi dictumst. Mauris rutrum porttitor et sit eget venenatis leo et. Posuere justo sed tristique viverra a. Quam velit faucibus sed eu leo sed sit.",
     image: certificateImage1,
   },
   {
-    name: "Judul Sertifikat",
+    key: 2,
+    title: "Judul Sertifikat",
     date: "12 Mei 2025",
     description:
       "Lorem ipsum dolor sit amet consectetur. Tellus viverra a id pellentesque eget vulputate. Vitae amet justo mi magna morbi montes vel amet. Bibendum pellentesque risus metus sagittis. Nisl sed tincidunt euismod habitasse vulputate quam donec morbi dictumst. Mauris rutrum porttitor et sit eget venenatis leo et. Posuere justo sed tristique viverra a. Quam velit faucibus sed eu leo sed sit.",
     image: certificateImage2,
   },
   {
-    name: "Judul Penghargaan",
+    key: 3,
+    title: "Judul Penghargaan",
     date: "12 Mei 2025",
     description:
       "Lorem ipsum dolor sit amet consectetur. Tellus viverra a id pellentesque eget vulputate. Vitae amet justo mi magna morbi montes vel amet. Bibendum pellentesque risus metus sagittis. Nisl sed tincidunt euismod habitasse vulputate quam donec morbi dictumst. Mauris rutrum porttitor et sit eget venenatis leo et. Posuere justo sed tristique viverra a. Quam velit faucibus sed eu leo sed sit.",
--- src/containers/certificates/index.tsx
+++ src/containers/certificates/index.tsx
@@ -2,6 +2,8 @@
 
 import { useTranslation } from "react-i18next";
 import classNames from "./certificates.module.scss";
+import { certificatesData } from "./data";
+import Image from "next/image";
 
 const CertificatesContainer = () => {
   const { t } = useTranslation();
@@ -9,6 +11,26 @@ const CertificatesContainer = () => {
     <div className={classNames.main}>
       <div className={classNames.content}>
         <p className={classNames.title}>{t("Sertifikat & Penghargaan")}</p>
+        <div className={classNames["certificates-wrapper"]}>
+          {certificatesData.map((item) => (
+            <div className={classNames.certificate} key={item.key}>
+              <div className={classNames.image}>
+                <Image
+                  src={item.image}
+                  alt="images"
+                  fill
+                  style={{ objectFit: "cover" }}
+                  className={classNames.img}
+                />
+              </div>
+              <div className={classNames.info}>
+                <p className={classNames.title}>{item.title}</p>
+                <p className={classNames.date}>{item.date}</p>
+                <p className={classNames.description}>{item.description}</p>
+              </div>
+            </div>
+          ))}
+        </div>
       </div>
     </div>
   );
