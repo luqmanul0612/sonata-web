a29c21850b22234f3d4478dd9e46e4cfd2f816f4
Muhammad Luqmanul Hakim <iamhkmid@gmail.com>
2025-05-13 16:15:31 +0700

    update

--- src/containers/projects/data.ts
+++ src/containers/projects/data.ts
@@ -45,20 +45,20 @@ export const projectsData = [
     label: "Portofolio",
     items: [
       {
-        key: 1,
-        title: "Proyek A",
-        location: "",
-        description:
-          "Lorem ipsum dolor sit amet consectetur. Ornare orci amet ut nunc tempor consectetur libero. Lectus nisl morbi libero convallis vel ipsum. Quisque donec diam odio adipiscing cursus aliquam vel. At turpis nec felis sed enim. Donec velit nunc et adipiscing sit diam bibendum nunc mi. Ullamcorper nulla id a non. Ipsum lacus vivamus vitae maecenas varius. Suspendisse sagittis suspendisse cras in tempor non nunc ultricies tristique. ",
-        image: projectA,
-      },
-      {
         key: 2,
         title: "Proyek B",
         location: "",
         description:
           "Lorem ipsum dolor sit amet consectetur. Ornare orci amet ut nunc tempor consectetur libero. Lectus nisl morbi libero convallis vel ipsum. Quisque donec diam odio adipiscing cursus aliquam vel. At turpis nec felis sed enim. Donec velit nunc et adipiscing sit diam bibendum nunc mi. Ullamcorper nulla id a non. Ipsum lacus vivamus vitae maecenas varius. Suspendisse sagittis suspendisse cras in tempor non nunc ultricies tristique. ",
         image: projectB,
+      },
+      {
+        key: 1,
+        title: "Proyek A",
+        location: "",
+        description:
+          "Lorem ipsum dolor sit amet consectetur. Ornare orci amet ut nunc tempor consectetur libero. Lectus nisl morbi libero convallis vel ipsum. Quisque donec diam odio adipiscing cursus aliquam vel. At turpis nec felis sed enim. Donec velit nunc et adipiscing sit diam bibendum nunc mi. Ullamcorper nulla id a non. Ipsum lacus vivamus vitae maecenas varius. Suspendisse sagittis suspendisse cras in tempor non nunc ultricies tristique. ",
+        image: projectA,
       },
     ],
   },
